ea2f08d53fb51869c32b6aa61a28cd30c95ae094
Denis Denisov <denis.denisov8701@gmail.com>
2024-08-28 10:18:42 +0200

    Initial Commit

---
+++ World/WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldApp.swift
+//  World
+//
+//  Created by Denis Denisov on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
